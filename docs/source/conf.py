9be10312c069ff2876514c84637d69f4b60c8754
Infometry <gh.admin@infometry.net>
2026-05-12 12:29:53 +0530

    Update Sphinx configuration for Retail Analytics project

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,35 +1,21 @@
-# Configuration file for the Sphinx documentation builder.
-
-# -- Project information
-
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
-
-release = '0.1'
-version = '0.1.0'
-
-# -- General configuration
+project = "Retail Analytics Text-to-SQL"
+copyright = "2026, Infometry-Infofiscus"
+author = "Infometry-Infofiscus"
+release = "0.1.0"
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "myst_parser",
 ]
 
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
 }
-intersphinx_disabled_domains = ['std']
-
-templates_path = ['_templates']
-
-# -- Options for HTML output
-
-html_theme = 'sphinx_rtd_theme'
-
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
